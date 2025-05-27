dbb5e0eaf2052dff12666fdac8223ba11d7b4fad
yandetoure <ndeyeyandemouhammad@gmail.com>
2025-05-27 15:50:57 +0000

    Ajout de boutons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -132,7 +132,7 @@
             right: 0;
             bottom: 0;
             background: rgba(0, 0, 0, 0.5);
-            z-index: 50;
+            z-index: 9999;
             opacity: 0;
             transition: opacity 0.3s ease-in-out;
         }
